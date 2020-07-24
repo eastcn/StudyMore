fad7caf7f1477d6cd2d404b45c726cf19e837df0
east <east_yxd@163.com>
2020-07-25 01:08:18 +0800

    除数博弈

--- src/main/java/LeetCode/main.java
+++ src/main/java/LeetCode/main.java
@@ -2,6 +2,7 @@ package LeetCode;
 
 
 import LeetCode.demo1.twoSum;
+import LeetCode.demo1025.Solution;
 import LeetCode.demo17_13.Respace;
 
 import java.util.Arrays;
@@ -9,13 +10,6 @@ import java.util.Arrays;
 
 public class main {
     public static void main(String[] args) {
-//        Respace respace = new Respace();
-//        String[] dict = {"looked", "just", "like", "her", "brother"};
-//        String sentence = "jesslookedjustliketimherbrother";
-//        System.out.println(respace.respace2(dict, sentence));
-        int[] nums = new int[]{-1,-2,-3,-4,-5};
-        int target = -8;
-        twoSum a = new twoSum();
-        System.out.println(Arrays.toString(a.twoSum2(nums, target)));
+        System.out.println(Solution.divisorGame(98));
     }
 }
