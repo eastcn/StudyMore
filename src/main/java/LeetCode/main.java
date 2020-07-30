36731a4291127a14f25a066edeb0670a61e170d3
east <east_yxd@163.com>
2020-07-30 21:56:09 +0800

    343. 整数拆除

--- src/main/java/LeetCode/main.java
+++ src/main/java/LeetCode/main.java
@@ -2,14 +2,14 @@ package LeetCode;
 
 
 import LeetCode.demo1.twoSum;
-import LeetCode.demo1025.Solution;
 import LeetCode.demo17_13.Respace;
+import LeetCode.demo343.Solution;
 
 import java.util.Arrays;
 
 
 public class main {
     public static void main(String[] args) {
-        System.out.println(Solution.divisorGame(98));
+        System.out.println(Solution.integerBreak(11));
     }
 }
