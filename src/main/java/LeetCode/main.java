874f52b439cd895907df59b780e7af037f93f3c5
east <east_yxd@163.com>
2020-07-31 11:03:03 +0800

    面试题08_03.魔术索引

--- src/main/java/LeetCode/main.java
+++ src/main/java/LeetCode/main.java
@@ -10,6 +10,6 @@ import java.util.Arrays;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println(Solution.integerBreak(11));
+        System.out.println();
     }
 }
