12ee4a7aefdcfc60e2d21e6e7e7f301a91863365
east <east_yxd@163.com>
2020-08-04 00:03:50 +0800

    413.字符串相加

---
+++ src/main/java/LeetCode/demo415/addString.java
@@ -0,0 +1,18 @@
+package LeetCode.demo415;
+
+/**
+ * @Classname addString
+ * @Description TODO
+ * @Date 2020/8/4 12:01 上午
+ * @Created by East
+ */
+public class addString {
+    public static String addString(String num1, String num2) {
+        // 字符串相加，双指针遍历，按每个位置的数相加，即模拟两数的竖式相加
+        return "1";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
