8dc98afb5f61a72f8b83a7bdd22d45e0256f93ee
east <east_yxd@163.com>
2020-08-10 13:46:55 +0800

    冒泡排序

---
+++ src/main/java/Interview/q01/Q1.java
@@ -0,0 +1,15 @@
+package Interview.q01;
+
+/**
+ * @Classname Q1
+ * @Description TODO
+ * @Date 2020/8/5 2:59 下午
+ * @Created by East
+ */
+public class Q1 {
+    public boolean isUnique(String astr) {
+//        int[] number = new int[astr.length()];
+
+        return false;
+    }
+}
