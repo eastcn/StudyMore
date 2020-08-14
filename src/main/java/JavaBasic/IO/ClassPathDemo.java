7e0ab5b5c0c20b23b30e3d33067aae590799aae8
east <east_yxd@163.com>
2020-08-14 15:09:56 +0800

    ClassPathDemo ReaderDemo

---
+++ src/main/java/JavaBasic/IO/ClassPathDemo.java
@@ -0,0 +1,22 @@
+package JavaBasic.IO;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @Classname ClassPathDemo
+ * @Description 从classpath中获取文件
+ * @Date 2020/8/13 11:20 下午
+ * @Created by East
+ */
+public class ClassPathDemo {
+    public static void main(String[] args) {
+        // 通过class.getResourceAsStream() 方法可以获取到classpath中的文件
+        try (InputStream inputStream = ClassPathDemo.class.getResourceAsStream("demoFile.txt")) {
+            byte[] buffer = new byte[1000];
+            System.out.println(inputStream.read(buffer));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
