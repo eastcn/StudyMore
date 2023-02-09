6471693af299b0bc62a5d7b6612f74cfab0fb2f8
east <east_yxd@163.com>
2023-02-09 14:05:46 +0800

    p1812

---
+++ src/main/java/com/east/LeetCode/problem206/Solution.java
@@ -0,0 +1,24 @@
+package com.east.LeetCode.problem206;
+
+import com.east.LeetCode.OfferProlem.offerProblem6.ListNode;
+
+/**
+ * @Classname Solution
+ * @Description TODO
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode pre = null;
+        ListNode nodeNow = head;
+        while (nodeNow != null) {
+            ListNode tempNode = nodeNow.next;
+            nodeNow.next = pre;
+
+            pre = nodeNow;
+            nodeNow = tempNode;
+        }
+        return pre;
+    }
+}
